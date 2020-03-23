2a9bdfc08f4f8ceb85ab4747296da3ec8a90c3d2
Elena Perrone <elenperrone11@gmail.com>
2020-03-23 15:06:31 +0100

    starter code

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+
+mongoose.connect(3000 , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(()=> {
+      console.log("Connected to db");
+  })
+  .catch((error)=> {
+      console.log("Not connected to db, error:", error);
+  })
+
+app.set('view engine', 'hbs');
+// app.set("views", __dirname + "/views"); we might need this
+// hbs.registerPartials(__dirname + '/views/partials'); comment out when use partials
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
