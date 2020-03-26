5ae7168baf0e44ff26ce2ede40a12bc8cbf4ed95
Neil Gursahani <gursahani.neil@gmail.com>
2020-03-27 00:08:38 +0100

    fourth day final commit

--- routes/tripPage.js
+++ routes/tripPage.js
@@ -3,21 +3,19 @@ const app = express();
 const tripModel = require('../models/Trip');
 const multer  = require('multer');
 const upload = multer({ dest: 'public/fileUploads'});
-
-// Render the trip page
-app.get('/trip', (request, response) => {
-    response.render('pages/tripPage');
-});
+const moment = require('moment');
 
 //Get information from the form
 app.post('/trip', upload.single('file'), (request, response) => {
+    const formatted_date = moment(request.body.date).format('DD-MM-YYYY');
     tripModel
         .create({
             country: request.body.country,
             city: request.body.city,
-            date: request.body.date,
+            date: formatted_date,
             file: request.file.filename,
             summary: request.body.summary,
+            rating: request.body.rating
         })
         .then((tripInfo) => {
             response.redirect('/trip');
@@ -32,6 +30,7 @@ app.get('/trip', (request, response) => {
     tripModel
         .find()
         .then((tripInfo) => {
+    
             response.render('pages/tripPage', {tripHbs: tripInfo});
         })
         .catch((error) => {
@@ -65,14 +64,21 @@ app.get('/trip/delete/:tripId', (request, response) => {
 
 //Update trip
 app.post('/trip/:tripId', (request, response) => {
-    console.log(request.body);
-    debugger
+    // console.log(request.body.date);
+    // let a = new Date(request.body.date);
+    // let formatted_date = `${a.getFullYear()}-${a.getMonth()}-${a.getDate()}`;
+
     tripModel
         .findByIdAndUpdate(request.params.tripId, {
             country: request.body.country,
             city: request.body.city,
             date: request.body.date,
-            summary: request.body.summary
+            summary: request.body.summary,
+            friends: request.body.friends,
+            food: request.body.food,
+            landmarks: request.body.landmarks,
+            rating: request.body.rating
+
         })
         .then((tripInfo) => {
             response.redirect(`/trip/${tripInfo._id}`);
