39eb5af542aa03604a4c013deed599c552e86b91
Elena Perrone <elenperrone11@gmail.com>
2020-03-26 22:27:26 +0100

    last today

--- routes/authentication/profile.js
+++ routes/authentication/profile.js
@@ -3,7 +3,14 @@ const app = express();
 const User = require("../../models/User");
 
 app.get("/profile", (req, res) => {
-  res.render("user/profile");
+  res.render("user/profile/", {profileData: req.session.currentUser});
 });
+
+// app.get("/profile/:id", (req, res) => {
+//   User.findById(req.params.id)
+//     .then((user)=> {
+//       res.render("user/profile/", {profileData: user});
+//     })
+// });
 
 module.exports = app;
